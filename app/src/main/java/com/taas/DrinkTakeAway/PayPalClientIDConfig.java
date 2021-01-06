ca20082259fe98d3d8a2eb846025242f248d515a
francesco-s <francescosannicola1997@gmail.com>
2021-01-06 20:14:33 +0100

    merge with paypal

---
+++ app/src/main/java/com/taas/DrinkTakeAway/PayPalClientIDConfig.java
@@ -0,0 +1,6 @@
+package com.taas.DrinkTakeAway;
+
+public class PayPalClientIDConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "Aav06doww9oKl6daarVYc2Uzvlm5w_FpkWS_Uh4QWCLT3Y3X3J5EWSoXJUJMBJkqrd-tnlYZ2SpBfJcQ";
+}
--- app/src/main/java/com/taas/DrinkTakeAway/ShoppingCart.java
+++ app/src/main/java/com/taas/DrinkTakeAway/ShoppingCart.java
@@ -1,12 +1,15 @@
 package com.taas.DrinkTakeAway;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +20,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
 import com.taas.DrinkTakeAway.adapter.CartAdapter;
 import com.taas.DrinkTakeAway.adapter.DrinkAdapter;
 import com.taas.DrinkTakeAway.models.Bevanda;
@@ -28,6 +36,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class ShoppingCart extends AppCompatActivity implements CartAdapter.onDrinkListenerCart{
@@ -43,6 +52,18 @@ public class ShoppingCart extends AppCompatActivity implements CartAdapter.onDri
 
     String localID;
     String userEmail;
+
+    float amount;
+    float total ;
+
+    private int PAYPAL_REQ_CODE = 12;
+    //public static final String PAYPAL_CLIENT_ID = "Aav06doww9oKl6daarVYc2Uzvlm5w_FpkWS_Uh4QWCLT3Y3X3J5EWSoXJUJMBJkqrd-tnlYZ2SpBfJcQ";
+    private static PayPalConfiguration paypalConfig = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)
+            .clientId(PayPalClientIDConfig.PAYPAL_CLIENT_ID);
+    //WE USE ENVIRONMENT SANDBOX, TEST PURPOSES ONLY
+    //ENVIRONMENT PRODUCTION IS USED WHEN THE APP IS READY FOR THE RELEASE
+
 
     final String serverAddress = "http://192.168.1.90:1111/api/v1/saveOrder";
 
@@ -68,14 +89,93 @@ public class ShoppingCart extends AppCompatActivity implements CartAdapter.onDri
         rvBevandeCart.setLayoutManager(new LinearLayoutManager(this));
 
 
+
+
+
+        total = 0;
+
+        for(int i=0;i<ordine.size();i++)
+        { total = total + ordine.get(i).getPrice(); }
+
+
+        /*
+         *
+         * PayPal payment section start
+         *
+         */
+
+        Intent payIntent = new Intent(this, PayPalService.class);
+        payIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+        startService(payIntent);
+
+        float finalTotal = total;
+
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                payPalPaymentMethod(finalTotal);
+                amount = finalTotal;
+
                 JSONObject jsonObj= buildJsonObj();
                 submitOrder(jsonObj);
             }
         });
 
+    }
+
+
+
+    private void payPalPaymentMethod(float totale) {
+
+        PayPalPayment pay = new PayPalPayment(new BigDecimal(totale),
+                "EUR", "Spike", PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(this, com.paypal.android.sdk.payments.PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, paypalConfig);
+        intent.putExtra(com.paypal.android.sdk.payments.PaymentActivity.EXTRA_PAYMENT, pay);
+
+        startActivityForResult(intent, PAYPAL_REQ_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //WE CHECK THE REQUEST CODE BECAUSE THERE MAY BE DIFFERENT OTHER CODES FOR OTHER ACTIVITIES
+        if(requestCode == PAYPAL_REQ_CODE)
+        {
+            if(resultCode == Activity.RESULT_OK)
+            {
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if (confirmation!=null)
+                {
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString(4);
+
+                        startActivity(new Intent(this, EndingPage.class)
+                                .putExtra("PaymentDetails", paymentDetails)
+                                .putExtra("PaymentAmount", amount)
+                        );  //startActivity method ends
+
+                    } catch (JSONException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+
+                Log.i("msg", "Payment successful");
+                Log.i("msg", "Pagato" + amount);
+            }
+            else if (resultCode == Activity.RESULT_CANCELED)
+            {
+                Log.i("msg", "Cancel");
+            }
+        }
+        else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
+        {
+            Log.i("msg", "Invalid");
+        }
     }
 
 
